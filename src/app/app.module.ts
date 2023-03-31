5e81d8c93b74c7fc972b36f0f6ddd50e1467f4c3
Alexandre <alehen.vieira@gmail.com>
2023-03-31 12:47:35 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { FormsModule } from '@angular/forms';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentosComponent,
+    ListarPensamentosComponent,
+    PensamentoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-criar-pensamentos',
+  templateUrl: './criar-pensamentos.component.html',
+  styleUrls: ['./criar-pensamentos.component.css']
+})
+export class CriarPensamentosComponent implements OnInit {
+
+  pensamento: Pensamento = {
+    conteudo:'',
+    autoria:'',
+    modelo: 'modelo1'
+  }
+
+  constructor(private service:  PensamentoService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    this.service.criar(this.pensamento).subscribe(() =>
+    this.router.navigate(['/listarPensamento']))
+  }
+  cancelar(){
+    this.router.navigate(['/listarPensamento'])
+  }
+}
